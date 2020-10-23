e34c84c2e632d78dda840c02d516f40e8a4d1b7d
Vincent Branchet <vince.branchet@gmail.com>
2020-10-23 17:39:07 +0200

    hotfix

--- js/Modules/Event/GameEventController.js
+++ js/Modules/Event/GameEventController.js
@@ -1,15 +1,11 @@
 class GameEventController extends AppChild {
     constructor(app) {
         super(app);
-
-        this.__macroController = new MacroEventController(this);
+        
         this.__nativeController = new NativeEventController(this);
         this.__recoController = new RecoEventController(this);
     }
 
-    get macroController() {
-        return this.__macroController;
-    }
     get nativeController() {
         return this.__nativeController;
     }
